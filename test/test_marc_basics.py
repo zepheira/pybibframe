ce8a0725b0b42683163ae7f87009f6c62979ba0c
Mark Baker <mark@zepheira.com>
2014-11-28 23:50:42 -0500

    Beginnings of test suite

--- test/test_marc_basics.py
+++ test/test_marc_basics.py
@@ -9,9 +9,12 @@ pip install pytest
 
 import sys
 import logging
+import asyncio
+import unittest
+from io import StringIO
 
 from versa.driver import memory
-from versa.util import jsondump
+from versa.util import jsondump, jsonload
 
 from bibframe.reader.marcxml import bfconvert
 
@@ -26,17 +29,45 @@ def module_path(local_function):
 #hack to locate test resource (data) files regardless of from where nose was run
 RESOURCEPATH = os.path.normpath(os.path.join(module_path(lambda _: None), '../resource/'))
 
-def test_basic_marc1():
-    #logging.debug('MARC BASICS PART 1')
-    m = memory.connection()
-    indoc = open(os.path.join(RESOURCEPATH, 'multiple-authlinks.xml'))
-    bfconvert([indoc], entbase='http://example.org/', model=m, config=None, verbose=False, logger=logging)
+class BasicTest(unittest.TestCase):
+    '''
+    Use a new event loop per test, and so one call of bfconvert per test
+    '''
 
-    jsondump(m, sys.stderr)
-    #logging.debug(recs)
-    assert m == ()
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
 
+    def test_model_consumed(self):
+        m = memory.connection()
+        with open(os.path.join(RESOURCEPATH, 'multiple-authlinks.xml')) as indoc:
+            bfconvert([indoc], entbase='http://example.org/', model=m, config=None, verbose=False, loop=self.loop)
+
+        assert m.size() == 0, 'Model not consumed: '+repr(m)
+
+    def test_simple_verify(self,name='gunslinger'):
+        m = memory.connection()
+        m_expected = memory.connection()
+        s = StringIO()
+        with open(os.path.join(RESOURCEPATH, name+'.mrx')) as indoc:
+            bfconvert(indoc, model=m, out=s, canonical=True, loop=self.loop)
+            s.seek(0)
+            jsonload(m, s)
+
+        with open(os.path.join(RESOURCEPATH, name+'.versa')) as indoc:
+            jsonload(m_expected, indoc)
+
+        with open('m-'+name,'w') as indoc:
+            indoc.write(repr(m))
+        with open('me-'+name,'w') as indoc:
+            indoc.write(repr(m_expected))
+        assert m==m_expected
+
+    def test_simple_verify2(self):
+        self.test_simple_verify(name='egyptskulls')
+
+    def test_simple_verify3(self):
+        self.test_simple_verify(name='kford-holdings1')
 
 if __name__ == '__main__':
     raise SystemExit("use py.test")
-
