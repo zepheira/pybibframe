60cd2fb5144713b1d87e43e55bbe3f41578af337
Uche Ogbuji <uche@ogbuji.net>
2015-01-10 23:25:21 -0700

    Fix import bug

--- lib/reader/util.py
+++ lib/reader/util.py
@@ -5,7 +5,8 @@ from enum import Enum #https://docs.python.org/3.4/library/enum.html
 from versa.pipeline import *
 from versa import I, VERSA_BASEIRI, ORIGIN, RELATIONSHIP, TARGET, ATTRIBUTES
 
-from bibframe.contrib.datachefids import slugify, default_idgen, FROM_EMPTY_HASH
+from bibframe.contrib.datachefids import slugify, FROM_EMPTY_HASH
+from bibframe.contrib.datachefids import idgen as default_idgen
 from bibframe import BFZ
 
 RDA_PARENS_PAT = re.compile('\\(.*\\)')
