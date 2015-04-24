d40cb80a1b542b6eff09936a226affb6c528d249
Mark Baker <mark@zepheira.com>
2015-04-24 14:06:24 -0400

    Define utility to merge/sort LoggedList instances used by a dict

--- lib/util.py
+++ lib/util.py
@@ -13,3 +13,29 @@ class LoggedList(list):
     def append(self, item):
         super(LoggedList, self).append(item)
         self.log.append((item, time.time()))
+
+def merge_list_logs(d):
+    '''
+    Given a dict, return the sorted, merged log of all values which
+    contain a log (primarily LoggedList)
+
+    >>> d = {}
+    >>> d['b']=LoggedList(['goodbye'])
+    >>> d['a']=LoggedList(['hello'])
+    >>> d['c']=LoggedList(['bonjour'])
+    >>> d['a'].append('au revoir')
+    >>> list(merge_list_logs(d))
+    [('b', 'goodbye', 0), ('a', 'hello', 0), ('c', 'bonjour', 0), ('a', 'au revoir', 1)]
+    >>> d['b'].append('ciao')
+    >>> list(merge_list_logs(d))
+    [('b', 'goodbye', 0), ('a', 'hello', 0), ('c', 'bonjour', 0), ('a', 'au revoir', 1), ('b', 'ciao', 1)]
+    '''
+
+    full_log = []
+    for k in d.keys():
+        if hasattr(d[k], 'log') and hasattr(d[k].log, '__iter__'):
+            full_log.extend((k, val, ind) for ind, val in enumerate(d[k].log))
+
+    full_log.sort(key=lambda x: x[1][1]) # sort by time
+
+    yield from ((k,v[0],i) for (k,v,i) in full_log)
