059bd18203134be8ef0e0b6e51ce8c5d790dcb2b
Uche Ogbuji <uche@ogbuji.net>
2015-09-21 12:58:16 -0600

    Fix logic of attr_class etc.

--- lib/reader/engine.py
+++ lib/reader/engine.py
@@ -87,12 +87,12 @@ def bfconvert(inputs, handle_marc_source=handle_marcxml_source, entbase=None, mo
 
     attr_cls = resolve_class(config.get('versa-attr-cls', 'builtins.dict'))
     attr_list_cls = resolve_class(config.get('versa-attr-list-cls', 'builtins.list'))
-    attr_ordered_cls = resolve_class(config.get('versa-attr-cls', 'collections.OrderedDict'))
+    #attr_ordered_cls = resolve_class(config.get('versa-attr-cls', 'collections.OrderedDict'))
 
     model_factory = functools.partial(memory.connection, attr_cls=attr_cls) #,logger=logger)
     model_factory.attr_list_cls = attr_list_cls
-    model_odict_factory = functools.partial(memory.connection, attr_cls=attr_ordered_cls) #,logger=logger)
-    model_odict_factory.attr_list_cls = attr_list_cls
+    #model_odict_factory = functools.partial(memory.connection, attr_cls=attr_ordered_cls) #,logger=logger)
+    #model_odict_factory.attr_list_cls = attr_list_cls
 
     if 'marc_record_handler' in config:
         handle_marc_source = AVAILABLE_MARC_HANDLERS[config['marc_record_handler']]
@@ -193,7 +193,7 @@ def bfconvert(inputs, handle_marc_source=handle_marcxml_source, entbase=None, mo
                 return cls
 
             args = dict(lax=lax)
-            handle_marc_source(source, sink, args, model_odict_factory)
+            handle_marc_source(source, sink, args, model_factory)
             sink.close()
             yield
         task = asyncio.async(wrap_task(), loop=loop)
